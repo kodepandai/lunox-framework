31dd6cd60aefc34b55cfc4d6203c75851fe6a58e
Akhmad Salafudin <axmad386@gmail.com>
2021-12-18 10:59:15 +0700

    perf(middleware): native middleware should be injected to 2nd params of middleware's next method

--- src/Contracts/Http/Middleware.ts
+++ src/Contracts/Http/Middleware.ts
@@ -1,8 +1,10 @@
 import type Request from "../../Http/Request";
+import type {Middleware as PolkaMiddleware} from "polka";
 
+export type NativeMiddleware = PolkaMiddleware
 export type MiddlewareHandler = (
   req: Request,
-  next: (req: Request) => void,
+  next: (req: Request, nativeMiddleware?: NativeMiddleware) => void,
   ...args: any[]
 ) => Promise<any>;
 
