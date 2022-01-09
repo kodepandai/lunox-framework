84d1bd446a43692c34884e71cd767a1c54ca7fc2
Akhmad Salafudin <axmad386@gmail.com>
2022-01-09 18:17:16 +0700

    feat(facade): added resolveFacadeInstance to resolve and save resolved facade

--- src/Support/Facades/Facade.ts
+++ src/Support/Facades/Facade.ts
@@ -1,10 +1,12 @@
 import type Application from "../../Foundation/Application";
-import type { Class } from "../../Types";
+import type { Class, ObjectOf } from "../../Types";
 
 abstract class Facade {
   protected static facadeId: string | null = null;
 
   protected static app: Application;
+
+  protected static resolvedInstance: ObjectOf<any> = {};
 
   public static setApplicationFacade(app: Application) {
     this.app = app;
@@ -16,18 +18,33 @@ abstract class Facade {
 
   static __getStatic(name: string, abstract: string) {
     return (...args: any) => {
-      let target = this as any;
-      if (typeof this.getFacadeAccessor() == "string") {
-        target = this.app.make(this.getFacadeAccessor() as string);
-      } else {
-        if (!this.app.instances[abstract]) {
-          this.app.singleton(abstract, this.getFacadeAccessor() as Class<any>);
-        }
-        target = this.app.make(abstract);
+      const target = this.resolveFacadeInstance(abstract);
+      if (target[name]) {
+        return target[name].call(target, ...args);
       }
-      return target[name].call(target, ...args);
     };
   }
+
+  protected static resolveFacadeInstance(abstract: string) {
+    if (typeof this.getFacadeAccessor() == "string") {
+      abstract = this.getFacadeAccessor() as string;
+    }
+    if (this.resolvedInstance[abstract]) {
+      return this.resolvedInstance[abstract];
+    }
+    let target: any;
+    if (typeof this.getFacadeAccessor() == "string") {
+      target = this.app.make(this.getFacadeAccessor() as string);
+    } else {
+      if (!this.app.instances[abstract]) {
+        this.app.singleton(abstract, this.getFacadeAccessor() as Class<any>);
+      }
+      target = this.app.make(abstract);
+    }
+    this.resolvedInstance[abstract] = target;
+    return target;
+  }
 }
+export class ExtendedFacade extends Facade {}
 
 export default Facade;
